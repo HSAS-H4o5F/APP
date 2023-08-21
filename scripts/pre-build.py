f3e5f2e0fb776275fe31ff0669818b3c5649e8f0
xfqwdsj <xfqwdsj@qq.com>
2023-08-21 21:27:36 +0800

    chore: 临时删除 Python 文件版权设置

--- scripts/pre-build.py
+++ scripts/pre-build.py
@@ -13,32 +13,6 @@
 #
 #  You should have received a copy of the GNU General Public License along with
 #  hsas_h4o5f_app. If not, see <https://www.gnu.org/licenses/>.
-#
-#  hsas_h4o5f_app is free software: you can redistribute it and/or modify it
-#  under the terms of the GNU General Public License as published by the Free
-#  Software Foundation, either version 3 of the License, or (at your option) any
-#  later version.
-#
-#  hsas_h4o5f_app is distributed in the hope that it will be useful, but WITHOUT
-#  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-#  FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
-#  details.
-#
-#  You should have received a copy of the GNU General Public License along with
-#  hsas_h4o5f_app. If not, see <https://www.gnu.org/licenses/>.
-#
-#  hsas_h4o5f_app is free software: you can redistribute it and/or modify it
-#  under the terms of the GNU General Public License as published by the Free
-#  Software Foundation, either version 3 of the License, or (at your option) any
-#  later version.
-#
-#  hsas_h4o5f_app is distributed in the hope that it will be useful, but WITHOUT
-#  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-#  FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
-#  details.
-#
-#  You should have received a copy of the GNU General Public License along with
-#  hsas_h4o5f_app. If not, see <https://www.gnu.org/licenses/>.
 
 import os
 import sys
@@ -46,6 +20,7 @@ import sys
 import yaml
 
 if len(sys.argv) > 1:
+
     file = open(os.path.abspath("pubspec.yaml"), "r")
     data = yaml.safe_load(file)
     file.close()
