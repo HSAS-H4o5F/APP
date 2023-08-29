9a057ce1a66a733e9cf221751e9b2059704f28a9
xfqwdsj <xfqwdsj@qq.com>
2023-08-30 02:13:29 +0800

    feat(Android): 实现旋转角度的 set 方法

--- android/app/src/main/kotlin/cn/edu/hsas/h4o5f_app/MainActivity.kt
+++ android/app/src/main/kotlin/cn/edu/hsas/h4o5f_app/MainActivity.kt
@@ -18,40 +18,47 @@
 
 package cn.edu.hsas.h4o5f_app
 
+import android.content.res.Configuration
 import android.os.Build
 import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.plugin.common.MethodChannel
 
 class MainActivity : FlutterActivity() {
-    private var orientationChannel: MethodChannel? = null;
+    private val rotation
+        get() = when {
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.R -> {
+                display?.rotation ?: 0
+            }
+
+            else -> {
+                @Suppress("DEPRECATION")
+                windowManager.defaultDisplay.rotation
+            }
+        }
+    private var rotationChannel: MethodChannel? = null;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        orientationChannel = flutterEngine?.dartExecutor?.let {
+        rotationChannel = flutterEngine?.dartExecutor?.let {
             MethodChannel(
                 it.binaryMessenger,
                 "hsas_h4o5f_app/screen_rotation"
             )
         }
-        orientationChannel?.setMethodCallHandler { call, result ->
+        rotationChannel?.setMethodCallHandler { call, result ->
             if (call.method == "get") {
-                result.success(Build.VERSION.SDK_INT.let {
-                    when {
-                        (it >= Build.VERSION_CODES.R) -> {
-                            @Suppress("NewApi")
-                            display?.rotation
-                        }
-                        else -> {
-                            @Suppress("DEPRECATION")
-                            windowManager.defaultDisplay.rotation
-                        }
-                    }
-                })
+                result.success(rotation)
             } else {
                 result.notImplemented()
             }
         }
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+
+        rotationChannel?.invokeMethod("set", rotation)
     }
 }
